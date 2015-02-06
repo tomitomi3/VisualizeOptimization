339aef72251e8d0cc98df8c4389f5b39b64b8180
tomitomi3 <tomi.nori+github@gmail.com>
2015-02-07 08:18:46 +0900

    1st comit

---
+++ VisualizeOptimization/Form1.Designer.vb
@@ -0,0 +1,145 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnInit = New System.Windows.Forms.Button()
+        Me.btnBack = New System.Windows.Forms.Button()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.oPlot = New OxyPlot.WindowsForms.Plot()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblIndex = New System.Windows.Forms.Label()
+        Me.tbxSkip = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.oneShotTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnInit
+        '
+        Me.btnInit.Location = New System.Drawing.Point(6, 18)
+        Me.btnInit.Name = "btnInit"
+        Me.btnInit.Size = New System.Drawing.Size(75, 23)
+        Me.btnInit.TabIndex = 1
+        Me.btnInit.Text = "Init"
+        Me.btnInit.UseVisualStyleBackColor = True
+        '
+        'btnBack
+        '
+        Me.btnBack.Location = New System.Drawing.Point(87, 18)
+        Me.btnBack.Name = "btnBack"
+        Me.btnBack.Size = New System.Drawing.Size(32, 23)
+        Me.btnBack.TabIndex = 2
+        Me.btnBack.Text = "<"
+        Me.btnBack.UseVisualStyleBackColor = True
+        '
+        'btnNext
+        '
+        Me.btnNext.Location = New System.Drawing.Point(125, 18)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(32, 23)
+        Me.btnNext.TabIndex = 3
+        Me.btnNext.Text = ">"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'oPlot
+        '
+        Me.oPlot.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.oPlot.Location = New System.Drawing.Point(0, 0)
+        Me.oPlot.Name = "oPlot"
+        Me.oPlot.PanCursor = System.Windows.Forms.Cursors.Hand
+        Me.oPlot.Size = New System.Drawing.Size(600, 600)
+        Me.oPlot.TabIndex = 1
+        Me.oPlot.Text = "Plot1"
+        Me.oPlot.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE
+        Me.oPlot.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE
+        Me.oPlot.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblIndex)
+        Me.GroupBox1.Controls.Add(Me.tbxSkip)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.btnInit)
+        Me.GroupBox1.Controls.Add(Me.btnNext)
+        Me.GroupBox1.Controls.Add(Me.btnBack)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(280, 68)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'lblIndex
+        '
+        Me.lblIndex.AutoSize = True
+        Me.lblIndex.Location = New System.Drawing.Point(6, 48)
+        Me.lblIndex.Name = "lblIndex"
+        Me.lblIndex.Size = New System.Drawing.Size(78, 12)
+        Me.lblIndex.TabIndex = 0
+        Me.lblIndex.Text = "Index aaa/bbb"
+        '
+        'tbxSkip
+        '
+        Me.tbxSkip.Location = New System.Drawing.Point(225, 20)
+        Me.tbxSkip.Name = "tbxSkip"
+        Me.tbxSkip.Size = New System.Drawing.Size(41, 19)
+        Me.tbxSkip.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(165, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(54, 12)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "SkipIndex"
+        '
+        'oneShotTimer
+        '
+        Me.oneShotTimer.Interval = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(600, 600)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.oPlot)
+        Me.Name = "Form1"
+        Me.Text = "VisualizeOptimization"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents btnInit As System.Windows.Forms.Button
+    Friend WithEvents btnBack As System.Windows.Forms.Button
+    Friend WithEvents btnNext As System.Windows.Forms.Button
+    Friend WithEvents oPlot As OxyPlot.WindowsForms.Plot
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents tbxSkip As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents lblIndex As System.Windows.Forms.Label
+    Friend WithEvents oneShotTimer As System.Windows.Forms.Timer
+
+End Class
---
+++ VisualizeOptimization/Form1.vb
@@ -0,0 +1,207 @@
+﻿Imports LibOptimization
+Imports OxyPlot.Series
+Imports OxyPlot
+Imports LibOptimization.Optimization
+
+Public Class Form1
+    Private opt As Optimization.clsOptNelderMead = Nothing
+    Private nowStepIndex As Integer = 0
+    Private vertexs As New List(Of List(Of List(Of Double)))
+    Private evals As New List(Of List(Of Double))
+
+    Declare Function AllocConsole Lib "kernel32.dll" () As Boolean
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'init plot
+        Me.oPlot.Model = New OxyPlot.PlotModel()
+        Me.oPlot.Model.PlotType = OxyPlot.PlotType.XY
+
+        'sample code
+        'Dim Series = New OxyPlot.Series.FunctionSeries(Function(x As Double) As Double
+        '                                                   Return 1 / Math.Sqrt(2 * Math.PI) * Math.Exp(-x * x / 2)
+        '                                               End Function, -100, 100, 0.1, "標準正規分布"
+
+        'init opt
+        Me.opt = New clsOptNelderMead(New BenchmarkFunction.clsBenchRosenblock(2))
+
+        Me.UpdateLabel()
+        Me.tbxSkip.Text = "1"
+
+        'oneshot timer
+        oneShotTimer.Start()
+
+        'alloc console
+        'AllocConsole()
+    End Sub
+
+    Private Sub btnInit_Click(sender As Object, e As EventArgs) Handles btnInit.Click
+        'init opt
+        Me.opt.Init(New Double()() {New Double() {0, 0}, New Double() {0, 1}, New Double() {1, 0}})
+
+        'Get simplex history
+        vertexs.Clear()
+        evals.Clear()
+        Dim tempSimplex As New List(Of List(Of Double))
+        Dim tempSimplexEvals As New List(Of Double)
+        For Each p As clsPoint In CType(opt, clsOptNelderMead).AllVertexs
+            Dim tempPoint As New clsPoint(p)
+            tempSimplex.Add(tempPoint)
+            tempSimplexEvals.Add(tempPoint.Eval)
+        Next
+        Me.vertexs.Add(tempSimplex)
+        Me.evals.Add(tempSimplexEvals)
+        While (Me.opt.DoIteration(1) = False)
+            tempSimplex = New List(Of List(Of Double))
+            tempSimplexEvals = New List(Of Double)
+            For Each p As clsPoint In CType(opt, clsOptNelderMead).AllVertexs
+                Dim tempPoint As New clsPoint(p)
+                tempSimplex.Add(tempPoint)
+                tempSimplexEvals.Add(tempPoint.Eval)
+            Next
+            Me.vertexs.Add(tempSimplex)
+            Me.evals.Add(tempSimplexEvals)
+        End While
+        Me.nowStepIndex = 0
+
+        Me.DrawInitAxis(-0.5, -0.5, 2, 2)
+    End Sub
+
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
+        If nowStepIndex <= 0 Then
+            Return
+        Else
+            nowStepIndex -= CInt(Me.tbxSkip.Text)
+            If nowStepIndex < 0 Then
+                nowStepIndex = 0
+            End If
+        End If
+        Me.Draw()
+    End Sub
+
+    Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+        If nowStepIndex + 1 >= vertexs.Count Then
+            Return
+        Else
+            nowStepIndex += CInt(Me.tbxSkip.Text)
+            If nowStepIndex >= Me.vertexs.Count Then
+                nowStepIndex = Me.vertexs.Count - 1
+            End If
+        End If
+        Me.Draw()
+    End Sub
+
+    Private Sub oneShotTimer_Tick(sender As Object, e As EventArgs) Handles oneShotTimer.Tick
+        oneShotTimer.Stop()
+        Me.DrawInitAxis(-1, -1, 2, 2, False)
+    End Sub
+
+    Private Sub oPlot_Paint(sender As Object, e As PaintEventArgs) Handles oPlot.Paint
+        'Console.WriteLine("{0},{1}", Me.oPlot.Model.DefaultXAxis.ActualMinimum, Me.oPlot.Model.DefaultXAxis.ActualMaximum)
+        'Dim zeroCrossXAxis = New LineSeries()
+        'zeroCrossXAxis.Points.Add(New DataPoint(Me.oPlot.Model.DefaultXAxis.ActualMinimum, 0))
+        'zeroCrossXAxis.Points.Add(New DataPoint(Me.oPlot.Model.DefaultXAxis.ActualMaximum, 0))
+        'zeroCrossXAxis.Color = OxyColors.Black
+        'zeroCrossXAxis.StrokeThickness = 0.5
+        'Me.oPlot.Model.Series.Add(zeroCrossXAxis)
+
+        'Dim zeroCrossYAxis = New LineSeries()
+        'zeroCrossYAxis.Points.Add(New DataPoint(0, Me.oPlot.Model.DefaultYAxis.ActualMinimum))
+        'zeroCrossYAxis.Points.Add(New DataPoint(0, Me.oPlot.Model.DefaultYAxis.ActualMaximum))
+        'zeroCrossYAxis.Color = OxyColors.Black
+        'zeroCrossYAxis.StrokeThickness = 0.5
+        'Me.oPlot.Model.Series.Add(zeroCrossYAxis)
+    End Sub
+
+    ''' <summary>
+    ''' Init axis
+    ''' </summary>
+    ''' <param name="ai_x"></param>
+    ''' <param name="ai_y"></param>
+    ''' <param name="ai_width"></param>
+    ''' <param name="ai_height"></param>
+    ''' <param name="ai_isDraw"></param>
+    ''' <remarks></remarks>
+    Private Sub DrawInitAxis(ByVal ai_x As Double, ByVal ai_y As Double, ByVal ai_width As Double, ByVal ai_height As Double, _
+                         Optional ByVal ai_isDraw As Boolean = True)
+        Me.oPlot.Model.Series.Clear()
+        Me.oPlot.Model.Axes.Clear()
+
+        'plot setting
+        Me.oPlot.BackColor = Color.White
+        Me.oPlot.Model.PlotMargins = New OxyThickness(0, 0, 0, 0)
+        Me.oPlot.Model.Padding = New OxyThickness(0, 0, 0, 0)
+
+        'set view
+        Dim x = New Axes.LinearAxis()
+        x.Position = Axes.AxisPosition.Bottom
+        x.Minimum = ai_x
+        x.Maximum = ai_x + ai_width
+        x.PositionAtZeroCrossing = True
+        'x.MajorGridlineStyle = LineStyle.Automatic
+        'x.MajorGridlineThickness = 0.5
+        'x.MaximumPadding = 0
+        'x.MinimumPadding = 0
+        Me.oPlot.Model.Axes.Add(x)
+
+        Dim y = New Axes.LinearAxis()
+        y.Position = Axes.AxisPosition.Left
+        y.Minimum = ai_y
+        y.Maximum = ai_y + ai_height
+        y.PositionAtZeroCrossing = True
+        'y.MajorGridlineStyle = LineStyle.Automatic
+        'y.MajorGridlineThickness = 0.5
+        'y.MaximumPadding = 0
+        'y.MinimumPadding = 0
+        Me.oPlot.Model.Axes.Add(y)
+
+        'Draw
+        If ai_isDraw = True Then
+            Me.Draw()
+        Else
+            Me.oPlot.InvalidatePlot(True)
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Draw
+    ''' </summary>
+    ''' <remarks></remarks>
+    Private Sub Draw()
+        Me.oPlot.Model.Series.Clear()
+
+        'Point
+        Dim points = New ScatterSeries()
+        points.Points.Add(New ScatterPoint(1, 1))
+        points.Points.Add(New ScatterPoint(1, 1))
+        points.MarkerSize = 2
+        points.TextColor = OxyColors.Black
+        Me.oPlot.Model.Series.Add(points)
+
+        'Line
+        Dim firstSeries = New LineSeries()
+        firstSeries.Points.Add(New DataPoint(Me.vertexs(nowStepIndex)(0)(0), Me.vertexs(nowStepIndex)(0)(1)))
+        firstSeries.Points.Add(New DataPoint(Me.vertexs(nowStepIndex)(1)(0), Me.vertexs(nowStepIndex)(1)(1)))
+        firstSeries.Points.Add(New DataPoint(Me.vertexs(nowStepIndex)(2)(0), Me.vertexs(nowStepIndex)(2)(1)))
+        firstSeries.Points.Add(New DataPoint(Me.vertexs(nowStepIndex)(0)(0), Me.vertexs(nowStepIndex)(0)(1)))
+        firstSeries.Color = OxyColors.Red
+        firstSeries.StrokeThickness = 1
+        Me.oPlot.Model.Series.Add(firstSeries)
+
+        Console.WriteLine("{0} , {1} , {2}", Me.evals(Me.nowStepIndex)(0), Me.evals(Me.nowStepIndex)(1), Me.evals(Me.nowStepIndex)(2))
+        Console.WriteLine(" {0} , {1}", vertexs(Me.nowStepIndex)(0)(0), vertexs(Me.nowStepIndex)(0)(1))
+        Console.WriteLine(" {0} , {1}", vertexs(Me.nowStepIndex)(1)(0), vertexs(Me.nowStepIndex)(1)(1))
+        Console.WriteLine(" {0} , {1}", vertexs(Me.nowStepIndex)(2)(0), vertexs(Me.nowStepIndex)(2)(1))
+
+        'update label
+        Me.UpdateLabel()
+        Me.oPlot.InvalidatePlot(True)
+    End Sub
+
+    Private Sub UpdateLabel()
+        If Me.vertexs.Count = 0 Then
+            Me.lblIndex.Text = String.Format("Step : {0}/{1}", 0, 0)
+        Else
+            Me.lblIndex.Text = String.Format("Step : {0}/{1}", Me.nowStepIndex + 1, Me.vertexs.Count)
+        End If
+    End Sub
+End Class
