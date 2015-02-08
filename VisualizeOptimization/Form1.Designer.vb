7dbb84fddd515958c59036c84134f4d9db28ce56
tomitomi3 <tomi.nori+github@gmail.com>
2015-02-08 20:01:22 +0900

    allow ControlPanel to move freely.

--- VisualizeOptimization/Form1.Designer.vb
+++ VisualizeOptimization/Form1.Designer.vb
@@ -23,44 +23,17 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.btnInit = New System.Windows.Forms.Button()
-        Me.btnBack = New System.Windows.Forms.Button()
-        Me.btnNext = New System.Windows.Forms.Button()
         Me.oPlot = New OxyPlot.WindowsForms.Plot()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.oneShotTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.ctrlPanel = New System.Windows.Forms.Panel()
         Me.lblIndex = New System.Windows.Forms.Label()
         Me.tbxSkip = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.oneShotTimer = New System.Windows.Forms.Timer(Me.components)
-        Me.GroupBox1.SuspendLayout()
+        Me.btnInit = New System.Windows.Forms.Button()
+        Me.btnNext = New System.Windows.Forms.Button()
+        Me.btnBack = New System.Windows.Forms.Button()
+        Me.ctrlPanel.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'btnInit
-        '
-        Me.btnInit.Location = New System.Drawing.Point(6, 18)
-        Me.btnInit.Name = "btnInit"
-        Me.btnInit.Size = New System.Drawing.Size(75, 23)
-        Me.btnInit.TabIndex = 1
-        Me.btnInit.Text = "Init"
-        Me.btnInit.UseVisualStyleBackColor = True
-        '
-        'btnBack
-        '
-        Me.btnBack.Location = New System.Drawing.Point(87, 18)
-        Me.btnBack.Name = "btnBack"
-        Me.btnBack.Size = New System.Drawing.Size(32, 23)
-        Me.btnBack.TabIndex = 2
-        Me.btnBack.Text = "<"
-        Me.btnBack.UseVisualStyleBackColor = True
-        '
-        'btnNext
-        '
-        Me.btnNext.Location = New System.Drawing.Point(125, 18)
-        Me.btnNext.Name = "btnNext"
-        Me.btnNext.Size = New System.Drawing.Size(32, 23)
-        Me.btnNext.TabIndex = 3
-        Me.btnNext.Text = ">"
-        Me.btnNext.UseVisualStyleBackColor = True
         '
         'oPlot
         '
@@ -75,71 +48,97 @@ Partial Class Form1
         Me.oPlot.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE
         Me.oPlot.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS
         '
-        'GroupBox1
+        'oneShotTimer
         '
-        Me.GroupBox1.Controls.Add(Me.lblIndex)
-        Me.GroupBox1.Controls.Add(Me.tbxSkip)
-        Me.GroupBox1.Controls.Add(Me.Label1)
-        Me.GroupBox1.Controls.Add(Me.btnInit)
-        Me.GroupBox1.Controls.Add(Me.btnNext)
-        Me.GroupBox1.Controls.Add(Me.btnBack)
-        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(280, 68)
-        Me.GroupBox1.TabIndex = 0
-        Me.GroupBox1.TabStop = False
+        Me.oneShotTimer.Interval = 1
+        '
+        'ctrlPanel
+        '
+        Me.ctrlPanel.Controls.Add(Me.lblIndex)
+        Me.ctrlPanel.Controls.Add(Me.tbxSkip)
+        Me.ctrlPanel.Controls.Add(Me.Label1)
+        Me.ctrlPanel.Controls.Add(Me.btnInit)
+        Me.ctrlPanel.Controls.Add(Me.btnNext)
+        Me.ctrlPanel.Controls.Add(Me.btnBack)
+        Me.ctrlPanel.Location = New System.Drawing.Point(12, 12)
+        Me.ctrlPanel.Name = "ctrlPanel"
+        Me.ctrlPanel.Size = New System.Drawing.Size(269, 56)
+        Me.ctrlPanel.TabIndex = 2
         '
         'lblIndex
         '
         Me.lblIndex.AutoSize = True
-        Me.lblIndex.Location = New System.Drawing.Point(6, 48)
+        Me.lblIndex.Location = New System.Drawing.Point(3, 33)
         Me.lblIndex.Name = "lblIndex"
         Me.lblIndex.Size = New System.Drawing.Size(78, 12)
-        Me.lblIndex.TabIndex = 0
+        Me.lblIndex.TabIndex = 6
         Me.lblIndex.Text = "Index aaa/bbb"
         '
         'tbxSkip
         '
-        Me.tbxSkip.Location = New System.Drawing.Point(225, 20)
+        Me.tbxSkip.Location = New System.Drawing.Point(222, 5)
         Me.tbxSkip.Name = "tbxSkip"
         Me.tbxSkip.Size = New System.Drawing.Size(41, 19)
-        Me.tbxSkip.TabIndex = 5
+        Me.tbxSkip.TabIndex = 11
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(165, 23)
+        Me.Label1.Location = New System.Drawing.Point(162, 8)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(54, 12)
-        Me.Label1.TabIndex = 4
+        Me.Label1.TabIndex = 10
         Me.Label1.Text = "SkipIndex"
         '
-        'oneShotTimer
+        'btnInit
         '
-        Me.oneShotTimer.Interval = 1
+        Me.btnInit.Location = New System.Drawing.Point(3, 3)
+        Me.btnInit.Name = "btnInit"
+        Me.btnInit.Size = New System.Drawing.Size(75, 23)
+        Me.btnInit.TabIndex = 7
+        Me.btnInit.Text = "Init"
+        Me.btnInit.UseVisualStyleBackColor = True
+        '
+        'btnNext
+        '
+        Me.btnNext.Location = New System.Drawing.Point(122, 3)
+        Me.btnNext.Name = "btnNext"
+        Me.btnNext.Size = New System.Drawing.Size(32, 23)
+        Me.btnNext.TabIndex = 9
+        Me.btnNext.Text = ">"
+        Me.btnNext.UseVisualStyleBackColor = True
+        '
+        'btnBack
+        '
+        Me.btnBack.Location = New System.Drawing.Point(84, 3)
+        Me.btnBack.Name = "btnBack"
+        Me.btnBack.Size = New System.Drawing.Size(32, 23)
+        Me.btnBack.TabIndex = 8
+        Me.btnBack.Text = "<"
+        Me.btnBack.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(600, 600)
-        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.ctrlPanel)
         Me.Controls.Add(Me.oPlot)
         Me.Name = "Form1"
         Me.Text = "VisualizeOptimization"
-        Me.GroupBox1.ResumeLayout(False)
-        Me.GroupBox1.PerformLayout()
+        Me.ctrlPanel.ResumeLayout(False)
+        Me.ctrlPanel.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
-    Friend WithEvents btnInit As System.Windows.Forms.Button
-    Friend WithEvents btnBack As System.Windows.Forms.Button
-    Friend WithEvents btnNext As System.Windows.Forms.Button
     Friend WithEvents oPlot As OxyPlot.WindowsForms.Plot
-    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents oneShotTimer As System.Windows.Forms.Timer
+    Friend WithEvents ctrlPanel As System.Windows.Forms.Panel
+    Friend WithEvents lblIndex As System.Windows.Forms.Label
     Friend WithEvents tbxSkip As System.Windows.Forms.TextBox
     Friend WithEvents Label1 As System.Windows.Forms.Label
-    Friend WithEvents lblIndex As System.Windows.Forms.Label
-    Friend WithEvents oneShotTimer As System.Windows.Forms.Timer
+    Friend WithEvents btnInit As System.Windows.Forms.Button
+    Friend WithEvents btnNext As System.Windows.Forms.Button
+    Friend WithEvents btnBack As System.Windows.Forms.Button
 
 End Class
--- VisualizeOptimization/Form1.vb
+++ VisualizeOptimization/Form1.vb
@@ -4,13 +4,19 @@ Imports OxyPlot
 Imports LibOptimization.Optimization
 
 Public Class Form1
-    Private opt As Optimization.clsOptNelderMead = Nothing
+    Private opt As Optimization.absOptimization = Nothing
     Private nowStepIndex As Integer = 0
     Private vertexs As New List(Of List(Of List(Of Double)))
     Private evals As New List(Of List(Of Double))
 
     Declare Function AllocConsole Lib "kernel32.dll" () As Boolean
 
+    ''' <summary>
+    ''' Load イベント
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    ''' <remarks></remarks>
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'init plot
         Me.oPlot.Model = New OxyPlot.PlotModel()
@@ -21,9 +27,6 @@ Public Class Form1
         '                                                   Return 1 / Math.Sqrt(2 * Math.PI) * Math.Exp(-x * x / 2)
         '                                               End Function, -100, 100, 0.1, "標準正規分布"
 
-        'init opt
-        Me.opt = New clsOptNelderMead(New BenchmarkFunction.clsBenchRosenblock(2))
-
         Me.UpdateLabel()
         Me.tbxSkip.Text = "1"
 
@@ -31,12 +34,13 @@ Public Class Form1
         oneShotTimer.Start()
 
         'alloc console
-        'AllocConsole()
+        AllocConsole()
     End Sub
 
     Private Sub btnInit_Click(sender As Object, e As EventArgs) Handles btnInit.Click
         'init opt
-        Me.opt.Init(New Double()() {New Double() {0, 0}, New Double() {0, 1}, New Double() {1, 0}})
+        Me.opt = New clsOptNelderMead(New BenchmarkFunction.clsBenchRosenblock(2))
+        Me.opt.Init()
 
         'Get simplex history
         vertexs.Clear()
@@ -63,7 +67,9 @@ Public Class Form1
         End While
         Me.nowStepIndex = 0
 
-        Me.DrawInitAxis(-0.5, -0.5, 2, 2)
+        Dim hoge() As Double = Me.GetEnvelope()
+
+        Me.DrawInitAxis(hoge(0), hoge(1), hoge(2), hoge(3))
     End Sub
 
     Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnBack.Click
@@ -177,15 +183,17 @@ Public Class Form1
         points.TextColor = OxyColors.Black
         Me.oPlot.Model.Series.Add(points)
 
+        'draw simplex
+
         'Line
-        Dim firstSeries = New LineSeries()
-        firstSeries.Points.Add(New DataPoint(Me.vertexs(nowStepIndex)(0)(0), Me.vertexs(nowStepIndex)(0)(1)))
-        firstSeries.Points.Add(New DataPoint(Me.vertexs(nowStepIndex)(1)(0), Me.vertexs(nowStepIndex)(1)(1)))
-        firstSeries.Points.Add(New DataPoint(Me.vertexs(nowStepIndex)(2)(0), Me.vertexs(nowStepIndex)(2)(1)))
-        firstSeries.Points.Add(New DataPoint(Me.vertexs(nowStepIndex)(0)(0), Me.vertexs(nowStepIndex)(0)(1)))
-        firstSeries.Color = OxyColors.Red
-        firstSeries.StrokeThickness = 1
-        Me.oPlot.Model.Series.Add(firstSeries)
+        Dim simplexPoint = New LineSeries()
+        simplexPoint.Points.Add(New DataPoint(Me.vertexs(nowStepIndex)(0)(0), Me.vertexs(nowStepIndex)(0)(1)))
+        simplexPoint.Points.Add(New DataPoint(Me.vertexs(nowStepIndex)(1)(0), Me.vertexs(nowStepIndex)(1)(1)))
+        simplexPoint.Points.Add(New DataPoint(Me.vertexs(nowStepIndex)(2)(0), Me.vertexs(nowStepIndex)(2)(1)))
+        simplexPoint.Points.Add(New DataPoint(Me.vertexs(nowStepIndex)(0)(0), Me.vertexs(nowStepIndex)(0)(1)))
+        simplexPoint.Color = OxyColors.Red
+        simplexPoint.StrokeThickness = 1
+        Me.oPlot.Model.Series.Add(simplexPoint)
 
         Console.WriteLine("{0} , {1} , {2}", Me.evals(Me.nowStepIndex)(0), Me.evals(Me.nowStepIndex)(1), Me.evals(Me.nowStepIndex)(2))
         Console.WriteLine(" {0} , {1}", vertexs(Me.nowStepIndex)(0)(0), vertexs(Me.nowStepIndex)(0)(1))
@@ -203,5 +211,41 @@ Public Class Form1
         Else
             Me.lblIndex.Text = String.Format("Step : {0}/{1}", Me.nowStepIndex + 1, Me.vertexs.Count)
         End If
+    End Sub
+
+    Private Function GetEnvelope() As Double()
+        'get envelope
+        Dim arx As New List(Of Double)
+        Dim ary As New List(Of Double)
+        arx.Add(1.0)
+        ary.Add(1.0)
+        For i As Integer = 0 To Me.vertexs(0).Count - 1
+            arx.Add(Me.vertexs(0)(i)(0))
+            ary.Add(Me.vertexs(0)(i)(1))
+        Next
+        arx.Sort()
+        ary.Sort()
+
+        Return New Double() {arx(0), ary(0), arx(arx.Count - 1) - arx(0), ary(ary.Count - 1) - ary(0)}
+    End Function
+
+    Dim isClick As Boolean = False
+    Dim previousPoint As Drawing.Point
+    Private Sub Panel1_MouseDown(sender As Object, e As MouseEventArgs) Handles ctrlPanel.MouseDown
+        isClick = True
+        previousPoint = e.Location
+    End Sub
+
+    Private Sub Panel1_MouseMove(sender As Object, e As MouseEventArgs) Handles ctrlPanel.MouseMove
+        If isClick Then
+            Dim movePoint As Drawing.Point
+            movePoint.X = ctrlPanel.Location.X + e.X - previousPoint.X
+            movePoint.Y = ctrlPanel.Location.Y + e.Y - previousPoint.Y
+            ctrlPanel.Location = movePoint
+        End If
+    End Sub
+
+    Private Sub Panel1_MouseUp(sender As Object, e As MouseEventArgs) Handles ctrlPanel.MouseUp
+        isClick = False
     End Sub
 End Class
